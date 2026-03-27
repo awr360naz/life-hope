c47e5150a245affcc836f1c7a8d158aa485e848a
sleman.s.97@gmail.com <sleman.s.97@gmail.com>
2026-03-27 09:04:34 +0300

    edit

--- src/pages/OurPicks.jsx
+++ src/pages/OurPicks.jsx
@@ -295,7 +295,7 @@ export default function OurPicks() {
           </FrameBox>
 
           {/* صورة 16:9 */}
-          <FrameBox title="اسئلة للمتابعين" className={`picks-area-image ${imageSrc ? "" : "is-disabled"}`}>
+          <FrameBox title="مقال" className={`picks-area-image ${imageSrc ? "" : "is-disabled"}`}>
           
             <a className="picks-link" href={imageHref} target="_blank" rel="noopener noreferrer">
               <div className="picks-media" style={{ aspectRatio: "16 /12 " }}>
